d6123120be6eb1a1b3f9d7d914489ef6a558f371
Chase Lindsay <chase.lindsay1@gmail.com>
2020-02-26 18:02:42 -0300

    finished restaurants controller and restaurants model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,8 @@
 class RestaurantsController < ApplicationController
 
+   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -10,10 +11,32 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :capacity, :category, :address, :price_range)
   end
+
 end
 
