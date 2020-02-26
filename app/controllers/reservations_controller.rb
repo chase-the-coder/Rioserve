07d044dd775a2d85fd8ee69fb6f4b1b3f83c581c
Lucas Cunha <lucasdinizcunha@uol.com.br>
2020-02-26 18:07:50 -0300

    Started implementing reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,46 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservation, only: %i[show destroy edit update]
+  def show
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+    @reservation.restaurant_id = params[:restaurant_id]
+    if @reservation.save
+      redirect_to root_path #should go to users show
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @reservation.update(reservation_params)
+      redirect_to reservation_path(params[:id])
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @reservation.delete
+    redirect_to root_path
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:number_of_people)
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :restaurants do
+    resources :reservations, except: :index
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
