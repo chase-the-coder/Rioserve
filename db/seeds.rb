6d8dbbd67ef94578204899d505f05f11e10721ef
Chase Lindsay <chase.lindsay1@gmail.com>
2020-02-27 16:58:49 -0300

    created restaurant show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Restaurant.delete_all
 20.times do
   Restaurant.create(
     name: Faker::Movies::LordOfTheRings.character,
-    user_id: 4,
+    user_id: 1,
     description: Faker::Restaurant.description,
     address: Faker::Movies::LordOfTheRings.location,
     rating: 5,
