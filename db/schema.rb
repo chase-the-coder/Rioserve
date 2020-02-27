d496030936c5abbde16f84df54239ef8ab6aed80
Chase Lindsay <chase.lindsay1@gmail.com>
2020-02-27 15:40:27 -0300

    added description to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_175435) do
+ActiveRecord::Schema.define(version: 2020_02_27_183909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_175435) do
     t.string "price_range"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
